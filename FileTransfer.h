a123306356e62040102cf2b2274624c8314a14ab
StefanKDS <stefseyer@gmail.com>
2019-05-16 13:49:40 +0200

    Update FileTransfer.h

--- FileTransfer.h
+++ FileTransfer.h
@@ -2,14 +2,18 @@
 #define FileTransfer_h
 
 #include "Arduino.h"
+#include <MCUFRIEND_kbv.h>
+#include "TouchScreen.h"
 
 class CFileTransfer
 {
     public:
         CFileTransfer();
-        bool Start(HardwareSerial* theserptr);
+        bool Start(MCUFRIEND_kbv* thetftptr, TouchScreen* thetsptr, HardwareSerial* theserptr);
 
     private:
+        MCUFRIEND_kbv* m_service_tftptr;
+        TouchScreen*   m_service_touchptr;
         HardwareSerial* m_service_serptr;
         void ReadFile (String theFileName);
 
